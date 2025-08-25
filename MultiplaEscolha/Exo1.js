472af21e61fbad51a4c7dd485d2db055ccaf0d7a
Urimash312 <davigabrielnos@Gmail.com>
2025-08-25 17:11:51 -0300

    sim
    sim

---
+++ MultiplaEscolha/Exo1.js
@@ -0,0 +1,32 @@
+var dia
+dia=parseInt(prompt("Digite o numero correspondente ao dia da semana"))
+switch(dia){
+    case 1:{alert("Domingo")
+        break
+    }
+    case 2:{alert("Segunda")
+        break
+    }
+    case 3:{alert("Terça-Feira")
+        break
+    }
+    
+    case 4:{alert("Quarta Feira")
+        break
+    }
+
+    case 5:{alert("Quinta Feira")
+        break
+    }
+    
+    case 6:{alert("Sexta-Feira")
+        break
+    }
+    case 7:{alert("Sabado")
+        break
+    }
+    default:{
+        (alert("Opçao invalida"))
+    }
+
+}
