7d717dad27f458ec1459b9dfd5c588801478d513
Urimash312 <davigabrielnos@Gmail.com>
2025-09-01 17:47:05 -0300

    Atividade8

---
+++ Atividade8/Atvidade.js
@@ -0,0 +1,11 @@
+var atual=1;
+var limite=21;
+while (atual<=limite) {
+    document.write(atual+" ");
+    atual++;
+    if (atual<=limite) {
+        document.write(atual+" ");
+        atual++;
+    }
+    atual++;
+}
